2b650457edbe4413a422248237018af3056f6816
mash <mash.act11@outlook.jp>
2019-02-17 14:14:22 +0900

    8.1.4

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,9 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
+      # ユーザーログイン後にユーザー情報のページにリダイレクトする
     else
+      flash[:danger] = 'Invalid email/password combination'
       render 'new'
     end
   end
